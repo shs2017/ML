19e6b86fb9fce3df78335ca0c66d7ba75fcd07c9
J H <jackson.harmon12@gmail.com>
2023-02-26 19:31:02 -0500

    (wip) code skeleton setup

--- policy_iteration/train.py
+++ policy_iteration/train.py
@@ -1,30 +1,8 @@
 import gymnasium as gym
 
 from model import Agent, PolicyIteration
+from context import Context
 from config import Config
-
-class Context:
-    """SARS context
-
-       S: state t
-       A: action
-       R: reward
-       S: state t + 1
-    """
-
-    def __init__(self, action, state, next_state, reward):
-        self.action = action
-        self.state = state
-        self.next_state = next_state
-        self.reward =reward
-
-    def __str__(self):
-        print('(' + \
-              f'action={self.action}, ' + \
-              f'state={self.state}, ' + \
-              f'next_state={self.next_state}, ' + \
-              f'reward={self.reward}, ' + \
-              ')')
 
 class Trainer:
     """Agent trainer"""
