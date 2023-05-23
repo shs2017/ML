3fcfc331519dfcaa51229430e176e0a62ec9a160
J H <jackson.harmon12@gmail.com>
2023-05-22 20:10:29 -0400

    fixed algorithm w.r.t. the episode end reward and increased code quality

--- model_free_rl/logger.py
+++ model_free_rl/logger.py
@@ -1,6 +1,10 @@
 from collections import deque
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 from config import get_config
+from typing import List, Tuple
 
 class RewardLogger:
     def __init__(self):
@@ -19,3 +23,12 @@ class RewardLogger:
         if self.config.log and (epoch % self.config.log_interval) == 0:
             average_reward = sum(self.rewards) / len(self.rewards)
             print(f'Epoch #{epoch}: Average Reward {average_reward}')
+
+
+def graph_policy_maps(policy_maps: Tuple[List[List[int]]]) -> None:
+    fig, ax = plt.subplots(ncols=len(policy_maps))
+    for i, policy_map in enumerate(policy_maps):
+        ax[i].imshow(policy_map)
+        ax[i].set_xticks(np.arange(1, 21))
+        ax[i].set_yticks(np.arange(1, 11))
+    plt.show()
