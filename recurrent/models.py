95238b93b305163750e2f919032c9f5604c4c257
J H <jackson.harmon12@gmail.com>
2023-08-22 21:13:49 -0400

    implemented an LSTM

--- recurrent/models.py
+++ recurrent/models.py
@@ -1,18 +1,20 @@
+import torch
+
 from torch import nn, Tensor
 
 
 class RNNCell(nn.Module):
-    def __init__(self, d_hid: int, d_state: int, d_out: int, out_is_logits: bool):
+    def __init__(self, d_in: int, d_hid: int, d_out: int, out_is_logits: bool):
         super().__init__()
 
         self.hidden_to_hidden = nn.Linear(d_hid, d_hid)
-        self.state_to_hidden = nn.Linear(d_state, d_hid)
+        self.state_to_hidden = nn.Linear(d_in, d_hid)
         self.hidden_to_output = nn.Linear(d_hid, d_out)
 
         self.sigma_hidden = nn.Tanh()
         self.sigma_out = nn.Identity() if out_is_logits else nn.Tanh()
 
-    def forward(self, state, hidden_state):
+    def forward(self, state: Tensor, hidden_state: Tensor) -> (Tensor, Tensor):
         hidden_from_hidden = self.hidden_to_hidden(hidden_state)
         hidden_from_state = self.state_to_hidden(state)
 
@@ -25,37 +27,133 @@ class RNNCell(nn.Module):
 
 
 class RNN(nn.Module):
-    def __init__(self, d_hid: int, d_state: int, d_out: int, n_layers: int):
+    def __init__(self, d_in: int, d_hid: int, d_out: int, n_layers: int):
         super().__init__()
 
+        self.d_in = d_in
         self.d_hid = d_hid
-        self.d_state = d_state
         self.d_out = d_out
 
         self.hidden_cells = nn.ModuleList([
             self._create_hidden_cell()
-            for _ in range(n_layers - 1)
+            for _ in range(n_layers)
         ])
-        self.output_cell = self._create_output_cell()
+        self.output_layer = nn.Linear(d_hid, d_out)
 
     def _create_hidden_cell(self):
-        return RNNCell(d_hid=self.d_hid, d_state=self.d_state,
-                       d_out=self.d_state, out_is_logits=False)
+        return RNNCell(d_in=self.d_in, d_hid=self.d_hid,
+                       d_out=self.d_in, out_is_logits=False)
 
-    def _create_output_cell(self):
-        return RNNCell(d_hid=self.d_hid, d_state=self.d_state,
-                       d_out=self.d_out, out_is_logits=True)
-
-    def forward(self, state: Tensor, hidden_states: [Tensor]) -> [Tensor]:
+    def forward(self, state: Tensor, hidden_states: [Tensor]) -> (Tensor,
+                                                                  [Tensor]):
         new_hidden_states = []
-
-        inner_states, last_hidden = hidden_states[:-1], hidden_states[-1]
-
-        for hidden_cell, hidden_state in zip(self.hidden_cells, inner_states):
+        for hidden_cell, hidden_state in zip(self.hidden_cells, hidden_states):
             state, hidden_state = hidden_cell(state, hidden_state)
             new_hidden_states.append(hidden_state)
 
-        output_state, hidden_state = self.output_cell(state, hidden_state)
-        new_hidden_states.append(hidden_state)
+        output_state = self.output_layer(state)
 
         return output_state, new_hidden_states
+
+
+class Gate(nn.Module):
+    def __init__(self, d_in: int, d_hidden, d_out: int):
+        super().__init__()
+
+        self.W_input_to_hidden = nn.Linear(d_in, d_out)
+        self.W_hidden_to_hidden = nn.Linear(d_hidden, d_out)
+
+    def forward(self, input_state, hidden_state):
+        input_hidden_logits = self.W_input_to_hidden(input_state)
+        hidden_hidden_logits = self.W_hidden_to_hidden(hidden_state)
+        return torch.sigmoid(hidden_hidden_logits + input_hidden_logits)
+
+
+class StateCombiner(nn.Module):
+    def __init__(self, d_in1, d_in2, d_out):
+        super().__init__()
+        self.proj1 = nn.Linear(d_in1, d_out)
+        self.proj2 = nn.Linear(d_in2, d_out)
+
+    def forward(self, state1: Tensor, state2: Tensor) -> Tensor:
+        return self.proj1(state1) + self.proj2(state2)
+
+
+class LSTMCell(nn.Module):
+    def __init__(self, d_in: int, d_hid: int, d_out: int):
+        super().__init__()
+
+        self.input_gate_fn = Gate(d_in, d_hid, d_hid)
+        self.output_gate_fn = Gate(d_in, d_hid, d_out)
+        self.forget_gate_fn = Gate(d_in, d_hid, d_hid)
+
+        self.new_cell_fn = StateCombiner(d_in, d_hid, d_hid)
+
+    def forward(self,
+                input_state: Tensor,
+                hidden_state: Tensor,
+                previous_cell: Tensor) -> (Tensor, [Tensor]):
+
+        input_gate = self.input_gate_fn(input_state, hidden_state)
+        output_gate = self.input_gate_fn(input_state, hidden_state)
+        forget_gate = self.input_gate_fn(input_state, hidden_state)
+
+        proposed_cell = self.new_cell_fn(input_state, hidden_state)
+
+        next_cell = forget_gate * previous_cell + input_gate * proposed_cell
+        next_hidden_state = output_gate * torch.tanh(next_cell)
+
+        return next_hidden_state, next_cell
+
+
+class LSTM(nn.Module):
+    def __init__(self, d_in: int, d_hid: int, d_out: int, n_layers: int):
+        super().__init__()
+
+        self.d_in = d_in
+        self.d_hid = d_hid
+        self.d_out = d_out
+
+        self.hidden_cells = nn.ModuleList([
+            self._create_hidden_cell()
+            for _ in range(n_layers)
+        ])
+
+        self.output_layer = nn.Linear(d_hid, d_out)
+
+    def _create_hidden_cell(self):
+        return LSTMCell(d_in=self.d_in, d_hid=self.d_hid, d_out=self.d_hid)
+
+    def forward(self,
+                state: Tensor,
+                hidden_states: [Tensor],
+                previous_cells: [Tensor]) -> (Tensor, [Tensor]):
+        next_cells = []
+        next_hidden_states = []
+        for hidden_cell, hidden_state, previous_cell in zip(self.hidden_cells,
+                                                            hidden_states,
+                                                            previous_cells):
+            hidden_state, cell = hidden_cell(state, hidden_state, previous_cell)
+            next_hidden_states.append(hidden_state)
+            next_cells.append(hidden_state)
+
+        output_state = self.output_layer(hidden_state)
+        return output_state, next_hidden_states, next_cells
+
+
+class GRUCell(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, state: Tensor, hidden_states: [Tensor]) -> (Tensor,
+                                                                  [Tensor]):
+        raise NotImplementedError
+
+
+class GRU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, state: Tensor, hidden_states: [Tensor]) -> (Tensor,
+                                                                  [Tensor]):
+        raise NotImplementedError
--- recurrent/train.py
+++ recurrent/train.py
@@ -4,27 +4,15 @@ import torch.nn.functional as F
 from torch.optim import SGD
 
 from dataset import X, Y
-from models import RNN
-
-
-rand = torch.rand(10)
-
-n_layers = 3
-d_hid = 10
-d_state = 10
-d_out = 10
-
-# rnn_cell = RNNCell(d_hid=d_hid, d_state=d_state,
-#                    d_out=d_out, out_is_logits=False)
-# out_cell = rnn_cell(rand, rand)
-# print(out_cell)
+from models import RNN, LSTM
 
 
 def create_initial_hidden_state(n_layers):
-    initial_state = torch.ones(d_state)
+    initial_state = torch.ones(d_in)
     return [initial_state.clone() for _ in range(n_layers)]
 
-def train(model, X, Y, n_epochs, optimizer):
+
+def train_rnn(model, X, Y, n_epochs, optimizer):
     n_samples = X.size(0)
 
     iterations = 0
@@ -47,7 +35,31 @@ def train(model, X, Y, n_epochs, optimizer):
         print(loss)
 
 
-def test(model, X):
+def train_lstm(model, X, Y, n_epochs, optimizer):
+    n_samples = X.size(0)
+
+    iterations = 0
+    for _ in range(n_epochs):
+        loss = 0.
+        cell = create_initial_hidden_state(n_layers)
+        hidden_state = create_initial_hidden_state(n_layers)
+        for i in range(n_samples):
+            x, y = X[i], Y[i]
+
+            output, hidden_state, cell = model(x, hidden_state, cell)
+            loss += F.binary_cross_entropy_with_logits(output, y)
+
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    iterations += 1
+
+    if iterations % 100 == 0:
+        print(loss)
+
+
+def test_rnn(model, X):
     n_samples = X.size(0)
 
     hidden_state = create_initial_hidden_state(n_layers)
@@ -58,9 +70,42 @@ def test(model, X):
         print(f'{torch.argmax(x)} -> {torch.argmax(output)}')
 
 
-n_epochs = 2_000
+def test_lstm(model, X):
+    n_samples = X.size(0)
 
-model = RNN(d_hid=d_hid, d_state=d_state, d_out=d_out, n_layers=n_layers)
-optimizer = SGD(model.parameters(), lr=1e-1)
-train(model, X, Y, n_epochs, optimizer)
-test(model, X)
+    cell = create_initial_hidden_state(n_layers)
+    hidden_state = create_initial_hidden_state(n_layers)
+    for i in range(n_samples):
+        x = X[i]
+        output, hidden_state, cell = model(x, hidden_state, cell)
+        output = torch.sigmoid(output)
+        print(f'{torch.argmax(x)} -> {torch.argmax(output)}')
+
+
+def train_and_test_rnn(d_in: int, d_hid: int, d_out: int, n_layers: int):
+    rnn_model = RNN(d_in=d_in, d_hid=d_hid, d_out=d_out, n_layers=n_layers)
+    rnn_optimizer = SGD(rnn_model.parameters(), lr=1e-1)
+    train_rnn(rnn_model, X, Y, n_epochs, rnn_optimizer)
+    test_rnn(rnn_model, X)
+
+
+def train_and_test_lstm(d_in: int, d_hid: int, d_out: int, n_layers: int):
+    lstm_model = LSTM(d_in=d_in, d_hid=d_hid, d_out=d_out, n_layers=n_layers)
+    lstm_optimizer = SGD(lstm_model.parameters(), lr=1e-1)
+    train_lstm(lstm_model, X, Y, n_epochs, lstm_optimizer)
+    test_lstm(lstm_model, X)
+
+
+if __name__ == '__main__':
+    n_epochs = 2_000
+    n_layers = 3
+
+    d_in = 10
+    d_hid = 10
+    d_out = 10
+
+    print('Training and test an RNN...')
+    train_and_test_rnn(d_in=d_in, d_hid=d_hid, d_out=d_out, n_layers=n_layers)
+
+    print('Training and test a LSTM...')
+    train_and_test_lstm(d_in=d_in, d_hid=d_hid, d_out=d_out, n_layers=n_layers)
